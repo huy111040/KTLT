cf8c00bdbf685808f938da99465a281fe49cc9b2
Nguyễn Huy <nghuy2368@gmail.com>
2024-11-20 14:12:13 +0700

    update

--- BT/Chuong6/BTCHUONG6/src/BT.java
+++ BT/Chuong6/BTCHUONG6/src/BT.java
@@ -1,11 +1,8 @@
 import java.util.Scanner;
 
 public class BT {
-    public static void inTamGiacVuong() {
-        Scanner sc = new Scanner(System.in);
-        System.out.printf("Nhập độ cao tam giác vuông");
-        int n = sc.nextInt();
-        for (int i = 0; i<= n; i++) {
+    public static void inTamGiacVuong(int n) {
+        for (int i = 0; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(" * ");
             }
@@ -13,11 +10,8 @@ public class BT {
         }
     }
 
-    public static void inTamGiacVuongNguoc() {
-        Scanner sc = new Scanner(System.in);
-        System.out.printf("Nhập độ cao tam giác vuông Ngược ");
-        int n = sc.nextInt();
-        for (int i = n; i> 0; i--) {
+    public static void inTamGiacVuongNguoc(int n) {
+        for (int i = n; i > 0; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(" * ");
             }
@@ -25,11 +19,8 @@ public class BT {
         }
     }
 
-    public static void inTamGiacVuongSo() {
-        Scanner sc = new Scanner(System.in);
-        System.out.printf("Nhập độ cao tam giác vuông bằng số ");
-        int n = sc.nextInt();
-        for (int i = 0; i< n+1; i++) {
+    public static void inTamGiacVuongSo(int n) {
+        for (int i = 0; i < n + 1; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(j + "  ");
             }
@@ -37,11 +28,8 @@ public class BT {
         }
     }
 
-    public static void inTamGiacVuongNguocSo() {
-        Scanner sc = new Scanner(System.in);
-        System.out.printf("Nhập độ cao tam giác vuông Ngược bằng số ");
-        int n = sc.nextInt();
-        for (int i = n; i> 0; i--) {
+    public static void inTamGiacVuongNguocSo(int n) {
+        for (int i = n; i > 0; i--) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(j + "  ");
             }
@@ -49,10 +37,7 @@ public class BT {
         }
     }
 
-    public static void inKimTuThap(){
-        Scanner sc = new Scanner(System.in);
-        System.out.printf("Nhập độ cao kim tự tháp ");
-        int n = sc.nextInt();
+    public static void inKimTuThap(int n) {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n - i; j++) {
                 System.out.print("  ");
@@ -65,10 +50,7 @@ public class BT {
 
     }
 
-    public static void inKimTuThapNguoc(){
-        Scanner sc = new Scanner(System.in);
-        System.out.printf("Nhập độ cao kim tự tháp ngược ");
-        int n = sc.nextInt();
+    public static void inKimTuThapNguoc(int n) {
         for (int i = n; i >= 1; i--) {
             for (int j = 1; j <= n - i; j++) {
                 System.out.print("  ");
@@ -81,10 +63,7 @@ public class BT {
 
     }
 
-    public static void inKimTuThapSo(){
-        Scanner sc = new Scanner(System.in);
-        System.out.printf("Nhập độ cao kim tự tháp ");
-        int n = sc.nextInt();
+    public static void inKimTuThapSo(int n) {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n - i; j++) {
                 System.out.print("   ");
@@ -93,17 +72,14 @@ public class BT {
                 System.out.print(k + "  ");
             }
             for (int k = i - 1; k >= 1; k--) {
-                System.out.print(k + "  "); 
+                System.out.print(k + "  ");
             }
             System.out.println();
         }
 
     }
 
-    public static void inKimTuThapSoNguoc(){
-        Scanner sc = new Scanner(System.in);
-        System.out.printf("Nhập độ cao kim tự tháp ngược ");
-        int n = sc.nextInt();
+    public static void inKimTuThapSoNguoc(int n) {
         for (int i = n; i >= 1; i--) {
             for (int j = 1; j <= n - i; j++) {
                 System.out.print("   ");
@@ -112,7 +88,7 @@ public class BT {
                 System.out.print(k + "  ");
             }
             for (int k = i - 1; k >= 1; k--) {
-                System.out.print(k + "  "); 
+                System.out.print(k + "  ");
             }
             System.out.println();
         }
@@ -120,7 +96,49 @@ public class BT {
     }
 
     public static void main(String[] args) {
-       inKimTuThapSoNguoc();
+        System.out.printf("Nhập độ cao tam giác : ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println();
+        System.out.println("Nhập lựa chọn:");
+        System.out.print("1. In tam giác vuông với ký tự *" +
+                "\n2. In tam giác vuông ngược với ký tự *" +
+                "\n3. In tam giác vuông với các số" +
+                "\n4. In tam giác vuông ngược với các số" +
+                "\n5. In kim tự tháp với ký tự * " +
+                "\n6. In kim tự tháp ngược với ký tự * " +
+                "\n7. In kim tự tháp với các số" +
+                "\n8. In kim tự tháp ngược với ký tự *" +
+                "\nLựa chọn của bạn là : ");
+        int choice = sc.nextInt();
+        switch (choice) {
+            case 1:
+                inTamGiacVuong(n);
+                break;
+            case 2:
+                inTamGiacVuongNguoc(n);
+                break;
+            case 3:
+                inTamGiacVuongSo(n);
+                break;
+            case 4:
+                inTamGiacVuongNguocSo(n);
+                break;
+            case 5:
+                inKimTuThap(n);
+                break;
+            case 6:
+                inKimTuThapNguoc(n);
+                break;
+            case 7:
+                inKimTuThapSo(n);
+                break;
+            case 8:
+                inKimTuThapSoNguoc(n);
+                break;
+            default:
+                System.out.println("Lựa chọn không hợp lệ!");
+        }
     }
 
 }
